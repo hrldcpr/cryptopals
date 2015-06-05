043b065facb7f063b0c0021d43669a78fdb9581c
Harold Cooper <hrldcpr@gmail.com>
2015-06-05 15:50:19 -0400

    clarify inputs

--- set1/challenge5.py
+++ set1/challenge5.py
@@ -14,7 +14,8 @@ def encrypt(bs, key):
 def main():
     x = b'''Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal'''
+    k = b'ICE'
 
-    y = encrypt(x, b'ICE')
+    y = encrypt(x, k)
 
     print(encode_hex(y) == b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
